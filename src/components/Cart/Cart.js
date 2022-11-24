945f2b06c90c52bb2d2f6d62e19eb58eb8f5ca55
LefebvreAnthony <anthony.lfb@hotmail.fr>
2022-11-24 18:42:40 +0100

    add logic in provider for add or remove item in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,8 +11,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
